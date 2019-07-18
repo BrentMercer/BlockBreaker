0435582a913eb80e395c15141a0ee31ca7eaa1de
Brent <hicx76@hotmail.com>
2019-07-17 20:46:04 -0600

    Initial commit

---
+++ Assets/Scripts/LevelManager.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class LevelManager : MonoBehaviour {
+
+	public void LoadLevel (string name) {
+		Debug.Log("Level load requested for: " + name);
+		Brick.breakableCount = 0;
+		Application.LoadLevel(name);
+	}
+
+	public void QuitRequest (string name) {
+		Debug.Log("I wanna quit");
+		Application.Quit();
+	}
+
+	public void MainMenu (string name) {
+		Debug.Log("Main menu");
+		Application.LoadLevel(name);
+	}
+
+	public void LoadNextLevel()	{
+		Brick.breakableCount = 0;
+		Application.LoadLevel(Application.loadedLevel + 1);
+	}
+
+	public void BrickDestroyed() {
+		if (Brick.breakableCount <= 0) {
+			LoadNextLevel();
+		}
+	}
+}
